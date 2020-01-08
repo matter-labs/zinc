8718b32668e013c27a122c46962ccdfcbbf09080
Alexander Movchan <am@matterlabs.dev>
2020-01-08 11:53:33 +0200

    Implement StoreSequenceByIndex

--- zinc/src/instructions/memory/store_array_by_index.rs
+++ zinc/src/instructions/memory/store_array_by_index.rs
@@ -1,5 +1,5 @@
 use crate::primitive::{Primitive, PrimitiveOperations};
-use crate::vm::VMInstruction;
+use crate::vm::{VMInstruction, Cell, InternalVM};
 use crate::vm::{RuntimeError, VirtualMachine};
 use zinc_bytecode::StoreSequenceByIndex;
 
@@ -8,7 +8,32 @@ impl<E, O> VMInstruction<E, O> for StoreSequenceByIndex
         E: Primitive,
         O: PrimitiveOperations<E>,
 {
-    fn execute(&self, _vm: &mut VirtualMachine<E, O>) -> Result<(), RuntimeError> {
-        unimplemented!()
+    fn execute(&self, vm: &mut VirtualMachine<E, O>) -> Result<(), RuntimeError> {
+        let index = vm.pop()?.value()?;
+
+        let mut array = Vec::with_capacity(self.array_len);
+        for i in 0..self.array_len {
+            let value = vm.load(self.address + i)?.value()?;
+            array.push(value);
+        }
+
+        let mut values = Vec::with_capacity(self.value_len);
+        for i in 0..self.value_len {
+            let value = vm.pop()?.value()?;
+            values.push(value);
+        }
+        values.reverse();
+
+        for (i, value) in values.into_iter().enumerate() {
+            let offset = vm.operations().constant_bigint(&i.into())?;
+            let address = vm.operations().add(index.clone(), offset)?;
+            array = vm.operations().array_set(array.as_slice(), address, value)?;
+        }
+
+        for (i, value) in array.into_iter().enumerate() {
+            vm.store(self.address + i, Cell::Value(value))?;
+        }
+
+        Ok(())
     }
 }
