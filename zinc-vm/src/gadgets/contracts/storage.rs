0945e0b1cdac3ce5249c6b634305d18328985b35
Ihor Barenblat <ib@matterlabs.dev>
2020-05-04 16:14:19 +0300

    Interface of storage gadget module v3

--- zinc-vm/src/gadgets/contracts/storage.rs
+++ zinc-vm/src/gadgets/contracts/storage.rs
@@ -4,24 +4,30 @@ use crate::{Engine, Result};
 use franklin_crypto::bellman::ConstraintSystem;
 use crate::stdlib::bits::signed_to_bits;
 use crate::core::RuntimeError;
+use num_integer::nth_root;
 
-pub struct StorageGadget<E: Engine> {
-    storage: &MerkleTreeStorage<E>,
+pub struct StorageGadget<'a, E: Engine, S: MerkleTreeStorage<E>> {
+    storage: &'a S,
     root_hash: Scalar<E>,
 }
 
-impl<E: Engine> StorageGadget<E> {
+impl<'a, E: Engine, S: MerkleTreeStorage<E>> StorageGadget<'a, E, S> {
     pub fn new<CS>(
         mut cs: CS,
-        storage: MerkleTreeStorage<E>,
+        storage: &'a S,
     ) -> Result<Self>
     where
         CS: ConstraintSystem<E>,
     {
-        let root_hash_value = storage.root_hash();
-        let root_hash = cs
-            .alloc_input(|| "root hash variable", || root_hash_value)
+        let root_hash_value = storage.root_hash()?;
+        let root_hash_variable = cs
+            .alloc_input(|| "root hash variable", || Ok(root_hash_value))
             .map_err(RuntimeError::SynthesisError)?;
+        let root_hash = Scalar :: <E> :: new_unchecked_variable(
+            Some(root_hash_value),
+            root_hash_variable,
+            ScalarType::Field,
+        );
         Ok(StorageGadget {
             storage,
             root_hash
@@ -36,7 +42,7 @@ impl<E: Engine> StorageGadget<E> {
     where
         CS: ConstraintSystem<E>,
     {
-
+        panic!("esv");
     }
 
     pub fn store<CS>(
@@ -48,7 +54,7 @@ impl<E: Engine> StorageGadget<E> {
     where
         CS: ConstraintSystem<E>,
     {
-
+        panic!("esv");
     }
 
     pub fn root_hash<CS>(
@@ -57,6 +63,6 @@ impl<E: Engine> StorageGadget<E> {
     where
         CS: ConstraintSystem<E>,
     {
-
+        panic!("esv");
     }
 }
